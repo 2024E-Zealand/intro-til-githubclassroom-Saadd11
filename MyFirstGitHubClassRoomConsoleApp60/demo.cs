906543fd955d05c92ba7f9ea6b08cf7e3f60924a
saadahmad <saa001@edu.zealand.dk>
2024-08-27 13:39:04 +0200

    push1

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,19 @@
+namespace MyFirstGitHubClassRoomConsoleApp60;
+
+public class demo
+{
+    public string Navn
+    {
+        get => navn;
+        set => navn = value;
+    }
+
+    public string Efternavn
+    {
+        get => efternavn;
+        set => efternavn = value;
+    }
+
+    public string navn;
+    public string efternavn;
+}
