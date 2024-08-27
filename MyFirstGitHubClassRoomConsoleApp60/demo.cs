cf47e3cdefefcb26e42f4fd9d4f14dea53d6a550
saadahmad <saa001@edu.zealand.dk>
2024-08-27 13:47:15 +0200

    push2

--- MyFirstGitHubClassRoomConsoleApp60/demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -2,18 +2,27 @@ namespace MyFirstGitHubClassRoomConsoleApp60;
 
 public class demo
 {
+    //instans
+    private string _navn;
+    private string _efternavn;
+    
+    // Prop
     public string Navn
     {
-        get => navn;
-        set => navn = value;
+        get { return _navn; }
+        set { _navn = value; }
     }
 
     public string Efternavn
     {
-        get => efternavn;
-        set => efternavn = value;
+        get { return _efternavn; }
+        set { _efternavn = value; }
     }
-
-    public string navn;
-    public string efternavn;
+    
+    //Kons
+    public demo(string navn, string efternavn)
+    {
+        _navn = navn;
+        _efternavn = efternavn;
+    }
 }
